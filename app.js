8ac35345635699268761469c768251fed1e2aaca
donato bencosme <donato bencosme>
2017-07-04 18:21:05 -0400

    primer commit chap_4

--- app.js
+++ app.js
@@ -1,22 +1,2 @@
-var fs = require("fs");
-var http = require("http");
-
-// fs.readFile("../jsEloquentSelf/talks.json", function(error,data){
-// 	console.log(JSON.parse(data.toString()));
-// });
-// console.log("Hola mundo");
-
-var content = `
-<html>
-Hello world
-<script>
-	alert('Hola mundo');
-</script>
-</html>
-`;
-http.createServer(function(request,response){
-	response.end(content);
-}).listen(8080);
-console.log("server running at http://localhost:8080");
 
 
